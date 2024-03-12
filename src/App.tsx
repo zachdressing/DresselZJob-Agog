04002e13bf8df76350da22deafbc12aa6eb5b25f
zachdressing <zed_dressel@hotmail.com>
2024-03-12 08:39:17 -0700

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { Routes, Route } from "react-router-dom"
+import Home from './Components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    );
+  }
 
 export default App;
